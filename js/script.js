407587e8defb3991d3e252d8afa173b3f1794bbb
Konstantin Revutsky <konstantin.rev@gmail.com>
2021-10-13 18:12:45 +0400

    fix: after review

--- js/script.js
+++ js/script.js
@@ -25,8 +25,6 @@ const initialCards = [
   }
 ];
 
-
-
 const popupEditButton = document.querySelector('.profile__info-button');
 const popupCreateButton = document.querySelector('.profile__button');
 const popupEdit = document.querySelector('#edit');
@@ -63,21 +61,35 @@ function handleClosePopupClick(evt) {
   } 
 }
 
+
+
+
+function errorsClear (popUp) {
+  const span = popUp.querySelectorAll('.error');
+  const button = popUp.querySelector('.popup__button');
+
+  span.forEach(function (element) {
+    element.textContent="";
+  });
+  button.disabled = false;
+}
+
 function openPopup (popUp) {
-    popUp.classList.add('popup_active');
-    page.addEventListener('mousedown', handleClosePopupClick);
-    document.addEventListener('keydown', closePopupKey);
+  popUp.classList.add('popup_active');
+  document.addEventListener('keydown', closePopupKey);
+  errorsClear(popUp);
 }
 
 function popupClose () {
   const activePopup = document.querySelector('.popup_active');
+ 
   if(activePopup) {
     activePopup.classList.remove('popup_active');
-    page.removeEventListener('mousedown', handleClosePopupClick);
     document.removeEventListener('keydown', closePopupKey);
-  } 
-      
+   } 
 }
+
+
 
 function renderValue() {
   webTitle.value = profileTitle.textContent;
@@ -91,20 +103,18 @@ function submitProfileForm (evt) {
 }
 function createGalleryElement(element) {
   const newGallery = galleryTemplateElement.content.cloneNode(true);
-
+  
   newGallery.querySelector('.gallery__description-title').textContent = element.name;
   newGallery.querySelector('.gallery__item-img').src = element.link;
-  newGallery.querySelector('.gallery__description-img').addEventListener('click', function (evt) {
-  evt.target.classList.toggle('gallery__description-img_selected');
-});
   newGallery.querySelector('.gallery__item-img').setAttribute('alt', element.name);
+  
   listenersGallery(newGallery);
   return newGallery;
 }
+
 function renderGallery () {
     initialCards.forEach(function (element) {
     const card = createGalleryElement(element);
-    listenersGallery(card);
     galleryContainerElement.append(card);
 
     });
@@ -119,28 +129,39 @@ function submitCardForm (event) {
   popupClose(popupCreate);
   event.currentTarget.reset();  
 }
+
 function deleteGallery (event) {
   const galleryItem = event.currentTarget.closest('.gallery__item');
   galleryItem.remove();
 }
+
 function listenersGallery (items) {
-  items.querySelector(".gallery__basket").addEventListener("click", deleteGallery);
+  items.querySelector(".gallery__basket").addEventListener('click', deleteGallery);
   items.querySelector(".gallery__item-img").addEventListener('click', doModalImage);
+  items.querySelector(".gallery__description-img").addEventListener('click', doLike);
+
 }
+
+function doLike (event) {
+   event.target.classList.toggle('gallery__description-img_selected');
+ }
+
 function doModalImage (event) {
   openPopup(popImg);
   const webImage = event.currentTarget.closest(".gallery__item-img");
   
   newImage.src = webImage.src;
+  newImage.alt = webImage.alt;
 
-  const txt = modalText.querySelector('.popup__profile');
+  const description = modalText.querySelector('.popup__profile');
 
   const newTitle = event.currentTarget.closest('.gallery__item');
   const newTitleContent = newTitle.querySelector('.gallery__description-title');
 
-  txt.textContent = newTitleContent.textContent;
+  description.textContent = newTitleContent.textContent;
 }
 
+page.addEventListener('mousedown', handleClosePopupClick);
 popImgClose.addEventListener('click', () => popupClose(popImg)); 
 popupEditButton.addEventListener('click', () => {
   openPopup(popupEdit);
